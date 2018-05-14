e27fcd65b3624a548d86a49e46f7c9f220af4194
EMex <erick00mex@gmail.com>
2018-05-14 00:40:56 -0500

    to heroku

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/pokedex/index.html'));
 });
 
 //Set Port
